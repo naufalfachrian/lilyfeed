f8844e0298257e59508449dabccd97febfe42ff1
Naufal Fachrian <20085705+naufalfachrian@users.noreply.github.com>
2023-06-20 20:27:15 +0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+let package = Package(
+    name: "LilyFeed",
+    platforms: [
+       .macOS(.v12)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/WebSubKit/websub-subscriber.git", from: "0.3.2"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "LilyFeed",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "WebSubSubscriber", package: "websub-subscriber"),
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://www.swift.org/server/guides/building.html#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .testTarget(name: "LilyFeedTests", dependencies: [
+            .target(name: "LilyFeed"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
