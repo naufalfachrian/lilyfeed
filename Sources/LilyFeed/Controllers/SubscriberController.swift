6bb24ec857ada4a0c5598cc2dd6c5b697bbc6ca4
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-06-21 20:32:46 +0700

    Update log info

--- Sources/LilyFeed/Controllers/SubscriberController.swift
+++ Sources/LilyFeed/Controllers/SubscriberController.swift
@@ -21,23 +21,23 @@ struct SubscriberController: SubscriberRouteCollection, UseRequestParser {
     
     func payload(for subscription: Subscription, on req: Request) async throws -> Response {
         req.logger.info(
-        """
-            Payload received on LilyFeed's userspace: \(req.body)
-        """
+            """
+            Payload received on LilyFeed's userspace from request: \(req.id)
+            """
         )
         switch try await self.parse(req, for: subscription) {
         case .success(let videos):
             req.logger.info(
-            """
-                \(videos.count) saved!
-            """
+                """
+                \(videos.count) saved from request: \(req.id)!
+                """
             )
             break
         case .failure(let reason):
             req.logger.info(
-            """
-                \(reason.localizedDescription) occurred parsing payload!
-            """
+                """
+                \(reason.localizedDescription) occurred when parsing payload on request: \(req.id)!
+                """
             )
             break
         }
