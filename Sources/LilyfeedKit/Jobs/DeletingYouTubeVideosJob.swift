61177959500c60987baf8e92a75e7abc496e1a75
Naufal Fachrian <naufal.fachrian@gmail.com>
2024-01-23 20:41:28 +0700

    Implement deleting YouTube videos job

---
+++ Sources/LilyfeedKit/Jobs/DeletingYouTubeVideosJob.swift
@@ -0,0 +1,29 @@
+//
+//  DeletingYouTubeVideosJob.swift
+//
+//
+//  Created by Bunga Mungil on 22/01/24.
+//
+
+import Foundation
+import Vapor
+import Queues
+import FluentKit
+
+
+public struct DeletingYouTubeVideosJob: AsyncJob {
+    
+    public init() {
+    }
+    
+    public typealias Payload = [String]
+    
+    public func dequeue(_ context: QueueContext, _ payload: [String]) async throws {
+        context.logger.info("Deleting YouTube Videos from storage -> \(payload.joined(separator: ", "))")
+        try await YouTubeVideoModel
+            .query(on: context.application.db)
+            .filter(\.$videoID ~~ payload)
+            .delete()
+    }
+    
+}
